31c2d4c877b37f351298a0193bf4d30c2394cc49
wachidaljawi <rwachid17@gmail.com>
2020-05-08 15:27:54 +0700

    create kerangka project

---
+++ toko_komputer/app/Http/Controllers/TokoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\toko;
+use Illuminate\Http\Request;
+
+class TokoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\toko  $toko
+     * @return \Illuminate\Http\Response
+     */
+    public function show(toko $toko)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\toko  $toko
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(toko $toko)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\toko  $toko
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, toko $toko)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\toko  $toko
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(toko $toko)
+    {
+        //
+    }
+}
