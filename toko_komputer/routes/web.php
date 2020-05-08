aaf220726977163b5370369cca4ce57e53506a49
wachidaljawi <rwachid17@gmail.com>
2020-05-08 22:51:36 +0700

    create route barang

--- toko_komputer/routes/web.php
+++ toko_komputer/routes/web.php
@@ -11,8 +11,18 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+/* route barang */
+Route::get('/barang', 'BarangController@index')->name('barang.index');
+Route::get('/barang/create', 'BarangController@create')->name('barang.create');
+Route::post('/barang', 'BarangController@store')->name('barang.store');
+Route::get('/barang/{barang}', 'BarangController@show')->name('barang.show');
+Route::get('/barang/{barang}/edit', 'BarangController@edit')->name('barang.edit');
+Route::put('/barang/{barang}', 'BarangController@update')->name('barang.update');
+Route::delete('/barang/{barang}', 'BarangController@destroy')->name('barang.destroy');
+// Route::resource('barang', 'BarangController');
